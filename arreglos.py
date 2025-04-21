25d1dcd9b6c2b64397c218d46e36bfa5e71d87b7
Toshy29 <toshyexs29@gmail.com>
2025-04-21 13:29:43 -0400

    Segundo commit (no entiendo nada de los arreglos)

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,13 @@
 # Comentario
+
+estudiantes = ["Estudiante1" , "Estudiante2"]
+estudiantescurso = ["Primero" , "Segundo"]
+estudiantesedad = [20 , 21]
+
+
+estudiantes = [
+    [ "nombre": "Estudiante1" , "edad" : 20 , "curso" : "Primero" ],
+    [ "nombre": "Estudiante2" , "edad" : 21 , "curso" : "Segundo"]
+
+]
+print(estudiantes)
